11428771e0507a6433d80f83bc268e4ecba71e0a
Giuliana-Cancino <martinezpajuelowalter@gmail.com>
2026-06-13 19:08:42 -0500

    Update UsuarioRepository.java

--- UsuarioRepository.java
+++ UsuarioRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+// comentario 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
